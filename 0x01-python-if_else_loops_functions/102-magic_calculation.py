20fe13dce34531c8af215e5e80134312ebb21869
Thami911 <thamimncwabe@gmail.com>
2023-06-06 15:06:33 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def magic_calculation(num0, num1, num2):
+    if num0 < num1:
+        return (num2)
+    if num2 > num1:
+        return (num0 + num1)
+    return (num0*num1 - num2)
